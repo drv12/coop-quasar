70ecc93fc1b6c0b5bf8d4b6b9d12e3a0302f460d
jmmacatangay <macatangayjohnmichaelangelo@gmail.com>
2020-02-16 20:38:47 +0800

    updated

---
+++ src/pages/AdminPages/AddStaff.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+      
+       <q-separator />
+       <div class="q-pa-md">
+       <q-card class="q-pa-md">
+         <q-card-section>
+             <div class="text-uppercase text-h3 text-teal">Office Staff</div>
+             <q-separator />
+             <div class="row">
+
+               <div class="col-lg-12"> 
+                   <div class="q-pa-md">       
+                        <q-input outlined color="teal-4" v-model="email" label="Email" />
+                   </div>
+                </div>
+
+                <div class="col-lg-12"> 
+                   <div class="q-pa-md">       
+                        <q-input v-model="password" outlined color="teal-4" :type="isPwd ? 'password' : 'text'" label="Password">
+                            <template v-slot:append>
+                            <q-icon
+                                :name="isPwd ? 'visibility_off' : 'visibility'"
+                                class="cursor-pointer"
+                                @click="isPwd = !isPwd"
+                            />
+                            </template>
+                        </q-input>
+                   </div>
+                </div>
+
+                <div class="col-lg-12 q-mb-xl"> 
+                   <div class="q-pa-md">       
+                        <q-select outlined color="teal-4" :options="roles" label="Role" />
+                   </div>
+                </div>
+
+                <div class="absolute-bottom-right q-mr-md"> 
+                   <div class="q-pa-md">       
+                         <q-btn class="text-teal-4" icon-right="check" label="Register Staff" color="white" />
+                   </div>
+                </div>
+
+             </div>
+          </q-card-section>
+       </q-card>
+       </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            roles: [ 'Admin', 'Collector'],
+            email: '',
+            password: '',
+            isPwd: true,
+        }
+    },
+}
+</script>
+<style scoped>
+    
+</style>
